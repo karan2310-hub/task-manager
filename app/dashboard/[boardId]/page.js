835bde1edc06d6b6378ab9291ab34d11a676d0b4
karan2310-hub <karan.backend@gmail.com>
2026-04-05 00:05:28 +0530

    dashboard boardid page.js mai update status ka fucntion added

--- app/dashboard/[boardId]/page.js
+++ app/dashboard/[boardId]/page.js
@@ -1,7 +1,8 @@
 "use client";
 import {useEffect, useState} from "react";
 
-export default function boardPage({params}){
+export default function boardPage({params}){//yaha wala param nextjs khud handle kr leta ha ia
+
  const[tasks,setTasks]=useState([]);
  const token =localStorage.getItem("token");
  const boardId=params.boardId();
@@ -16,10 +17,24 @@ export default function boardPage({params}){
     const data = await res.json();
     setTasks(data);
  };
+ const updateStatus = async (taskId, status) => {
+  await fetch("/api/tasks/update", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({ taskId, status })
+  });
+
+  //  update ke baad tasks dubara lao
+  getTasks();
+};
 
  useEffect(()=>{
     getTasks();
  },[]);
+
 
  return (
     <div>
@@ -29,8 +44,19 @@ export default function boardPage({params}){
         <div key={task._id}>
           <p>{task.title}</p>
           <p>{task.status}</p>
+
+          {/* now for ui of status update */}
+          <select
+  value={task.status}
+  onChange={(e) => updateStatus(task._id, e.target.value)}
+>
+  <option value="todo">Todo</option>
+  <option value="in-progress">In Progress</option>
+  <option value="done">Done</option>
+</select>
         </div>
       ))}
+
     </div>
   );
 }
